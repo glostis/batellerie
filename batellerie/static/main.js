46d60611f3dc9967e619f41e4d978b2972b0a0ae
Guillaume Lostis <guillaume@lostis.com>
2024-12-04 17:57:19 +0100

    Color scaling

--- batellerie/static/main.js
+++ batellerie/static/main.js
@@ -189,6 +189,9 @@ function updateTimestampLive(tsMax) {
 }
 
 function drawTracks(tracks, tsMax, tsMin) {
+  const colorScale = d3
+    .scaleSequential(d3.interpolateOrRd)
+    .domain([tsMin, tsMax]);
   Object.values(tracks).forEach((coordinates) => {
     if (coordinates.length > 1) {
       const latLngs = coordinates.map(([lat, lon, ts]) => [lat, lon]);
@@ -196,8 +199,9 @@ function drawTracks(tracks, tsMax, tsMin) {
 
       for (let i = 0; i < latLngs.length - 1; i++) {
         const opacity = Math.max((tss[i] - tsMin) / (tsMax - tsMin), 0.05);
+        const color = colorScale(tss[i]);
         L.polyline([latLngs[i], latLngs[i + 1]], {
-          color: "#F8591F",
+          color,
           weight: 3,
           opacity,
           lineCap: "butt",
@@ -227,19 +231,24 @@ function addShipMarkers(positions, tsMax, tsMin, live) {
 
 function createShipMarker(lat, lon, course, speed, ts, tsMax, tsMin) {
   // Decreasing opacity, depending on the freshness of the data
+  const colorScale = d3
+    .scaleSequential(d3.interpolateOrRd)
+    .domain([tsMin, tsMax]);
+  console.log(tsMin, ts, tsMax);
   const opacity = Math.max((ts - tsMin) / (tsMax - tsMin), 0.15);
   return course !== null && speed !== 0
     ? L.marker([lat, lon], {
         icon: L.divIcon({
           className: "arrow-icon",
-          html: `<div style="transform: rotate(${course || 0}deg); opacity: ${opacity}"><img src="static/arrow_icon.svg">
+          html: `<div style="transform: rotate(${course || 0}deg); opacity: ${opacity}">
+<svg version="1.0" width="20" height="20" viewBox="0 0 1280 1280" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g transform="matrix(-0.1,0,0,0.1,1280.0495,0.09709988)" fill="${colorScale(ts)}" stroke="none" id="g1"><path d="M 314,12790 C 119,12749 -21,12548 5,12345 11,12294 388,11534 3045,6220 5946,419 6081,151 6127,110 6188,56 6284,11 6358,4 c 118,-13 258,40 334,125 31,35 771,1508 3070,6106 2924,5849 3029,6062 3035,6126 15,173 -76,326 -237,403 -59,27 -74,30 -160,30 -79,-1 -104,-5 -150,-26 -30,-13 -1359,-894 -2953,-1956 L 6400,8880 3503,10812 c -1594,1062 -2923,1942 -2953,1956 -61,27 -168,37 -236,22 z" id="path1"/></g></svg>
 </div>`,
           iconSize: [20, 20],
         }),
       })
     : L.circleMarker([lat, lon], {
         radius: 5,
-        fillColor: "#F8591F",
+        fillColor: colorScale(ts),
         stroke: false,
         fillOpacity: opacity,
       });
